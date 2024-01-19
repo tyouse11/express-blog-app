daf3d206fa492a86b1db6bf063481d01ae70a000
tyouse1 <tyouse1@live.com>
2024-01-19 10:40:18 -0500

    moved all routes to their own router files

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const app = express();
 const port = 3000;
 
 // Importing the data from our fake database files.
-const users = require("./data/users");
-const posts = require("./data/posts");
+const users = require("./routes/users");
+const posts = require("./routes/posts");
 
 // We import the body-parser package.
 // This package contains middleware that can handle
@@ -19,34 +19,144 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({ extended: true }));
 
-app
-  .route("/api/users")
-  .get((req, res) => {
-    res.json(users);
-  })
-  .post((req, res) => {
-    // Within the POST request route, we create a new
-    // user with the data given by the client.
-    // We should also do some more robust validation here,
-    // but this is just an example for now.
-    if (req.body.name && req.body.username && req.body.email) {
-      if (users.find((u) => u.username == req.body.username)) {
-        res.json({ error: "Username Already Taken" });
-        return;
-      }
+// // New logging middleware to help us keep track of
+// // requests during testing!
+// app.use((req, res, next) => {
+//   const time = new Date();
 
-      const user = {
-        id: users[users.length - 1].id + 1,
-        name: req.body.name,
-        username: req.body.username,
-        email: req.body.email,
-      };
+//   console.log(
+//     `-----
+// ${time.toLocaleTimeString()}: Received a ${req.method} request to ${req.url}.`
+//   );
+//   if (Object.keys(req.body).length > 0) {
+//     console.log("Containing the data:");
+//     console.log(`${JSON.stringify(req.body)}`);
+//   }
+//   next();
+// });
 
-      users.push(user);
-      res.json(users[users.length - 1]);
-    } else res.json({ error: "Insufficient Data" });
-  });
+// app
+//   .route("/api/users")
+//   .get((req, res) => {
+//     res.json(users);
+//   })
+//   .post((req, res) => {
+//     // Within the POST request route, we create a new
+//     // user with the data given by the client.
+//     // We should also do some more robust validation here,
+//     // but this is just an example for now.
+//     if (req.body.name && req.body.username && req.body.email) {
+//       if (users.find((u) => u.username == req.body.username)) {
+//         res.json({ error: "Username Already Taken" });
+//         return;
+//       }
 
+//       const user = {
+//         id: users[users.length - 1].id + 1,
+//         name: req.body.name,
+//         username: req.body.username,
+//         email: req.body.email,
+//       };
+
+//       users.push(user);
+//       res.json(users[users.length - 1]);
+//     } else res.json({ error: "Insufficient Data" });
+//   });
+
+//   app
+//   .route("/api/users/:id")
+//   .get((req, res, next) => {
+//     const user = users.find((u) => u.id == req.params.id);
+//     if (user) res.json(user);
+//     else next();
+//   })
+//   .patch((req, res, next) => {
+//     // Within the PATCH request route, we allow the client
+//     // to make changes to an existing user in the database.
+//     const user = users.find((u, i) => {
+//       if (u.id == req.params.id) {
+//         for (const key in req.body) {
+//           users[i][key] = req.body[key];
+//         }
+//         return true;
+//       }
+//     });
+
+//     if (user) res.json(user);
+//     else next();
+//   })
+//   .delete((req, res, next) => {
+//     // The DELETE request route simply removes a resource.
+//     const user = users.find((u, i) => {
+//       if (u.id == req.params.id) {
+//         users.splice(i, 1);
+//         return true;
+//       }
+//     });
+
+//     if (user) res.json(user);
+//     else next();
+//   });
+
+// app
+//   .route("/api/posts")
+//   .get((req, res) => {
+//     res.json(posts);
+//   })
+//   .post((req, res) => {
+//     // Within the POST request route, we create a new
+//     // post with the data given by the client.
+//     if (req.body.userId && req.body.title && req.body.content) {
+//       const post = {
+//         id: posts[posts.length - 1].id + 1,
+//         userId: req.body.userId,
+//         title: req.body.title,
+//         content: req.body.content,
+//       };
+
+//       posts.push(post);
+//       res.json(posts[posts.length - 1]);
+//     } else res.json({ error: "Insufficient Data" });
+//   });
+
+// app
+//   .route("/api/posts/:id")
+//   .get((req, res, next) => {
+//     const post = posts.find((p) => p.id == req.params.id);
+//     if (post) res.json(post);
+//     else next();
+//   })
+//   .patch((req, res, next) => {
+//     // Within the PATCH request route, we allow the client
+//     // to make changes to an existing post in the database.
+//     const post = posts.find((p, i) => {
+//       if (p.id == req.params.id) {
+//         for (const key in req.body) {
+//           posts[i][key] = req.body[key];
+//         }
+//         return true;
+//       }
+//     });
+
+//     if (post) res.json(post);
+//     else next();
+//   })
+//   .delete((req, res, next) => {
+//     // The DELETE request route simply removes a resource.
+//     const post = posts.find((p, i) => {
+//       if (p.id == req.params.id) {
+//         posts.splice(i, 1);
+//         return true;
+//       }
+//     });
+
+//     if (post) res.json(post);
+//     else next();
+//   });
+
+// app.get("/", (req, res) => {
+//   res.send("Work in progress!");
+// });
 
 // All routes
 // // Creating a GET route for the entire users database.
@@ -88,6 +198,39 @@ app
 //     res.status(404);
 //     res.json({ error: "Resource Not Found" });
 //   });
+
+
+// Use our Routes
+
+// Use our Routes
+
+// Logging Middlewaare
+app.use((req, res, next) => {
+  const time = new Date();
+
+  console.log(
+    `-----
+${time.toLocaleTimeString()}: Received a ${req.method} request to ${req.url}.`
+  );
+  if (Object.keys(req.body).length > 0) {
+    console.log("Containing the data:");
+    console.log(`${JSON.stringify(req.body)}`);
+  }
+  next();
+});
+
+app.use("/api/users", users);
+app.use("/api/posts", posts);
+
+app.get("/", (req, res) => {
+  res.send("Work in progress!");
+});
+
+// 404 Middleware
+app.use((req, res) => {
+  res.status(404);
+  res.json({ error: "Resource Not Found" });
+});
 
 app.listen(port, () => {
   console.log(`Server listening on port: ${port}.`);
